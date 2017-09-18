36fa0f624a5b881db257f9dffe4cf6ff23ff8804
“ChrisWen960216 <957638221@qq.com>
2017-09-18 13:53:35 +0800

    Webstorm test push

--- test/ejs/index.js
+++ test/ejs/index.js
@@ -1,13 +1,22 @@
 const path = require('path');
 const ejs = require('ejs');
 
-const html = `Hello<%- World%>`;
-const World = 'xxx';
+const html = `Hello<%- World %>`;
+//const World = 'xxx';
 
+/*
+ * EJS.compile ===> 将字符串转换成 Function
+ * locals => 'hello' + locals.World
+ */
 const f1 = ejs.compile(html);
 console.log(f1);
 
+/* <% %> 逻辑运算
+ * <%- %> unescape
+ * <%= %> escape
+ */
+
 const finalStr = f1({
-    World
+    World: 'Great'
 });
 console.log(finalStr);
