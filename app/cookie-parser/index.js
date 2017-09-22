7df5f46db299566b05a9ff319eee32d2324c3483
“ChrisWen960216 <957638221@qq.com>
2017-09-22 13:11:17 +0800

    Change code queue

--- app/cookie-parser/index.js
+++ app/cookie-parser/index.js
@@ -8,17 +8,19 @@ module.exports = ctx => {
     let {url} = ctx.req;
     let {cookie} = ctx.req.headers;
     let {resCtx, res} = ctx;
+    const whiteNameList = ['/name_ChrisWen'];
+
     let cookieObj = cookie_parser.parse(cookie);
+    const cookieStr = time => `authd=hello;Max-Age=${time}`;
     return Promise.resolve({
         then: (resolve, reject) => {
             //实现白名单
             if (cookieObj['authd']) {
                 resCtx.hasUser = true;
+                res.setHeader('Set-Cookie', cookieStr(3600));
             }
-            const whiteNameList = ['/name_ChrisWen'];
-            let cookieStr = time => `authd=hello;Max-Age=${time}`;
             if (whiteNameList.indexOf(url) > -1) {
-                res.setHeader('Set-Cookie', cookieStr(36000));
+                res.setHeader('Set-Cookie', cookieStr(3600));
             }
             if (url === '/logout') {
                 res.setHeader('Set-Cookie', cookieStr(0));
